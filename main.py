bbd2c81842f6ee037fe1f07c85852ffca7d25a15
Louis Lacoste <louislacoste33@gmail.com>
2019-01-05 06:50:06 +0100

    ajout score et collision balle raquette

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 from tkinter import *
-import time
+import tkinter.font as tkFont
 
 class Ball:
 
     def __init__(self, tk, color):
-        self.x = 3
-        self.y = 3
+        self.x = -3
+        self.y = -3
 
         self.field = {"height" : canvas.winfo_height(), "width" : canvas.winfo_width()}
 
         self.canvas = canvas
-        self.form = canvas.create_oval(390, 390, 410, 410, fill = color)
+        self.form = canvas.create_oval(890, 390, 910, 410, fill = color)
     def draw(self):
         self.canvas.move(self.form, self.x, self.y)
 
     def position(self):
         position = self.canvas.coords(self.form)
         return position
-
-
-
-
 
 
 class Raquette:
@@ -49,11 +45,14 @@ def move_raquette(e):
     if e.keysym == "q" or e.keysym == "Q":
         raquette2.move(raquette2.down)
 
+
+
+
 def draw():
 
     ball.draw()
 
-    # BALLE ET COLLISION MURS
+    #*****BALLE ET COLLISION MURS*****
 
     if (ball.position()[0] <= 0 or ball.position()[2] >= ball.field["width"]):
         ball.x *= -1
@@ -61,9 +60,27 @@ def draw():
     if (ball.position()[1] <= 0 or ball.position()[3] >= ball.field["height"]):
         ball.y *= -1
 
-    # RAQUETTES ET MOUVEMENT
+    #*****COLLISION BALLE RAQUETTE*****
+    rayon_balle = (ball.position()[2] - ball.position()[0])/2
+    #*RAQUETTE GAUCHE*
 
-    print(raquette1.position())
+    if(ball.position()[0] < canvas.winfo_width() * (3/32) and ball.position()[0] > canvas.winfo_width() * (3/32) - 5):
+
+        if(ball.position()[1]+rayon_balle > raquette1.position()[1] and ball.position()[1]+rayon_balle < raquette1.position()[3]):
+
+            print(True)
+            ball.x *= -1
+
+    #*RAQUETTE DROITE
+
+    if (ball.position()[2] > canvas.winfo_width() * (29 / 32) and ball.position()[2] < canvas.winfo_width() * (29 / 32) + 5):
+
+        if (ball.position()[3] - rayon_balle > raquette2.position()[1] and ball.position()[1] - rayon_balle < raquette2.position()[3]):
+
+            print(True)
+            ball.x *= -1
+
+    #*****RAQUETTES ET MOUVEMENT RAQUETTES*****
 
     if(raquette1.position()[1] < 0):
         raquette1.move(raquette1.position()[1] * -1)
@@ -77,8 +94,20 @@ def draw():
     if (raquette2.position()[3] > canvas.winfo_height()):
         raquette2.move((raquette2.position()[3] - canvas.winfo_height()) * -1)
 
+    #*****POINTS*****
+
+    #*AFFICHAGE DES POINTS*
+    score_droite = 0
+    score_gauche = 0
+
+    font = tkFont.Font(family="FixedSys", size=80, weight="bold", slant="roman")
+    canvas.create_text(canvas.winfo_width()*(3/8), canvas.winfo_height()*(1/8),font=font, fill="white", text=score_gauche)
+
+    canvas.create_text(canvas.winfo_width()*(5/8), canvas.winfo_height()*(1/8),font=font, fill="white", text=score_droite)
+
     tk.after(10, draw)
 
+#*****PRESSETS FENETRE TK*****
 
 tk = Tk()
 tk.title("Pong Game")
@@ -88,7 +117,7 @@ canvas.pack()
 tk.update()
 
 
-#ligne du milieu
+#*****LIGNE MILIEU*****
 
 for i in range(0, 30):
     if(i % 2 == 0):
@@ -96,26 +125,29 @@ for i in range(0, 30):
     else:
         canvas.create_line(canvas.winfo_width()/2, canvas.winfo_height() * ((5+i)/40), canvas.winfo_width()/2, canvas.winfo_height() * ((5+i)/40) +20, fill="#888", width=2)
 
-#lignes but
+#*LIGNES DE BUT
 
 canvas.create_line(0, 0, 0, canvas.winfo_height(), width=20, fill="#AAA")
 canvas.create_line(canvas.winfo_width(), 0, canvas.winfo_width(), canvas.winfo_height(), width=20, fill="#AAA")
 
-
+#*****LIGNES TERRAIN JOUEURS
 
 canvas.create_line(canvas.winfo_width() * (3/32), 0, canvas.winfo_width() * (3/32), canvas.winfo_height(), fill="white")
 canvas.create_line(canvas.winfo_width() * (29/32), 0, canvas.winfo_width() * (29/32), canvas.winfo_height(), fill="white")
 
-
-
+#*****DESSIN BALLE*****
 ball = Ball(tk, "#fff")
+
+#*****DESSIN RAQUETTES JOUEURS*****
 
 raquette1 = Raquette(tk, "#fff", canvas.winfo_width() * (3/32), canvas.winfo_height() * (1/16))
 
+#*AJOUT DES CONTROLES DES RAQUETTES
 tk.bind_all("<Key>", move_raquette)
 
 raquette2 = Raquette(tk, "#fff", canvas.winfo_width() * (29/32), canvas.winfo_height() * (1/16))
 
+#*FRAME*
 draw()
 
 tk.mainloop()
