4ec82e64f655bb86defd73c86f1b5c26c8209209
Louis Lacoste <louislacoste33@gmail.com>
2019-01-05 02:03:50 +0100

    ajout ligne milien et lignes buts

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ class Ball:
         position = self.canvas.coords(self.form)
         return position
 
+class Raquette:
+
+    def __init__(self):
+        pass
+
 
 tk = Tk()
 tk.title("Pong Game")
@@ -27,7 +32,21 @@ canvas = Canvas(tk, width=1280, height=720, bg="#000")
 canvas.pack()
 tk.update()
 
-canvas.create_line(canvas.winfo_width()/2, canvas.winfo_height() * (1/10), canvas.winfo_width()/2, canvas.winfo_height() * (9/10), width=2, fill="#fff", dash=(40, 10))
+#ligne du milieu
+
+for i in range(0, 30):
+    print(True)
+    if(i % 2 == 0):
+        pass
+    else:
+        canvas.create_line(canvas.winfo_width()/2, canvas.winfo_height() * ((5+i)/40), canvas.winfo_width()/2, canvas.winfo_height() * ((5+i)/40) +20, fill="#888", width=2)
+
+#lignes but
+
+canvas.create_line(0, 0, 0, canvas.winfo_height(), width=20, fill="#AAA")
+canvas.create_line(canvas.winfo_width(), 0, canvas.winfo_width(), canvas.winfo_height(), width=20, fill="#AAA")
+
+
 
 ball = Ball(tk, "#fff")
 
@@ -36,12 +55,7 @@ ball.y = 3
 
 while True:
 
-    #BALLE ET COLLISION MURS 
-
-    print(ball.x, ball.field["width"])
-    print(ball.y, ball.field["height"])
-
-    print(ball.position())
+    #BALLE ET COLLISION MURS
 
     if(ball.position()[0] <= 0 or ball.position()[2] >= ball.field["width"]):
         ball.x *= -1
@@ -49,7 +63,10 @@ while True:
     if (ball.position()[1] <= 0 or ball.position()[3] >= ball.field["height"]):
         ball.y *= -1
 
-    ball.draw()
 
+
+
+
+    ball.draw()
     tk.update()
     time.sleep(0.01)
